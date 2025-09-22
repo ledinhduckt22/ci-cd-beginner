d23193417ca5acd9f1df3422722667d5f722bab2
Aaron Le <dinhduca7@gmail.com>
2025-09-23 00:09:17 +0700

    Update message response for api

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello CI/CD!');
+app.get("/", (req, res) => {
+  res.send("Hello CI/CD Day 1!");
 });
 
 app.listen(PORT, () => {
